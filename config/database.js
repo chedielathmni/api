2b2e9984b244d51d33bffabe44f58f24214c043f
Hamza Hajji <hamza.hajji221@gmail.com>
2020-05-30 21:05:04 +0100

    Updated config/database.js - Added database connection check

--- config/database.js
+++ config/database.js
@@ -1,12 +1,24 @@
-const { createPool } = require("mysql");
+const mysql = require('mysql');
 
-const pool = createPool({
+// Database Credentials and Settings
+const dbSettings = {
   port: process.env.DB_PORT,
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.MYSQL_DB,
   connectionLimit: 10
+}
+
+const connection = mysql.createConnection(dbSettings);
+
+connection.connect(function (err) {
+  if (err) {
+    return console.error(`Unable to connect to database ${process.env.DB_HOST}:${process.env.DB_PORT}`);
+  }
+  console.log(`Connected to the MySQL server at ${process.env.DB_HOST}:${process.env.DB_PORT}`);
 });
+
+const pool = mysql.createPool(dbSettings);
 
 module.exports = pool;
