d3f8fcf3d393a53e95dc5505b904f3ba96e0f4e7
Hamza Hajji <hamza.hajji221@gmail.com>
2020-05-30 21:06:27 +0100

    Updated api/users/user.service.js - Code refactoring and using functions instead of direct module exports

--- api/users/user.service.js
+++ api/users/user.service.js
@@ -1,60 +1,126 @@
 const pool = require("../../config/database");
 
-module.exports = {
-    create: (data, callBack) => {
-    pool.query(
-      `insert into transporter(first_name, last_name, password, phone_number) 
-                values(?,?,?,?)`,
-      [
-        data.firstName,
-        data.lastName,
-        data.password,
-        data.phoneNumber
-      ],
-      (error, results, fields) => {
-        if (error) {
-          callBack(error);
-        }
-        return callBack(null, results);
-      }
-    );
-  },
-  getUserByPhoneNumber: (phoneNumber, callBack) => {
-    pool.query(
-      `select * from transporter where phone_number = ?`,
-      [phoneNumber],
-      (error, results, fields) => {
-        if (error) {
-          callBack(error);
-        }
-        return callBack(null, results[0]);
-      }
-    );
-  },
-  getUserByUserId: (id, callBack) => {
-    pool.query(
-      `select * from transporter where id = ?`,
-      [id],
-      (error, results, fields) => {
-        if (error) {
-          callBack(error);
-        }
-        return callBack(null, results[0]);
-      }
-    );
-  },
-  getUsers: callBack => {
-    pool.query(
-      `select * from transporter`,
-      [],
-      (error, results, fields) => {
-        if (error) {
-          callBack(error);
-        }
-        return callBack(null, results);
-      }
-    );
-  },
+// Create user
+function create(data, callBack) {
+  // Insert user query
+  const query = `insert into transporter(first_name, last_name, password, phone_number) 
+  values(?,?,?,?)`;
+
+  // Query values
+  const values = [
+    data.firstName,
+    data.lastName,
+    data.password,
+    data.phoneNumber
+  ];
+
+  pool.query(query, values, (error, results) => {
+    if (error) {
+      callBack(error);
+    }
+    return callBack(null, results);
+  }
+  );
+}
+
+// Get user by phone number
+function getUserByPhoneNumber(phoneNumber, callBack) {
+  // Select by phone number query
+  const query = `select * from transporter where phone_number = ?`;
+
+  pool.query(query, [phoneNumber], (error, results) => {
+    if (error) {
+      callBack(error);
+    }
+    return callBack(null, results[0]);
+  }
+  );
+}
+
+// Get user by ID
+function getUserByUserId(id, callBack) {
+  // Select by ID query
+  const query = `select * from transporter where id = ?`
+
+  pool.query(query, [id], (error, results) => {
+    if (error) {
+      callBack(error);
+    }
+    return callBack(null, results[0]);
+  }
+  );
+}
+
+// Fetch all users
+function getUsers(callBack) {
+  // Select all users query
+  const query = `select id, first_name, last_name, phone_number from transporter`;
+  pool.query(query, [], (error, results) => {
+    if (error) {
+      callBack(error);
+    }
+    return callBack(null, results);
+  }
+  );
+}
+
+module.exports = { create, getUserByPhoneNumber, getUserByUserId, getUsers };
+
+// module.exports = {
+//   create: (data, callBack) => {
+//     pool.query(
+//       `insert into transporter(first_name, last_name, password, phone_number) 
+//                   values(?,?,?,?)`,
+//       [
+//         data.firstName,
+//         data.lastName,
+//         data.password,
+//         data.phoneNumber
+//       ],
+//       (error, results, fields) => {
+//         if (error) {
+//           callBack(error);
+//         }
+//         return callBack(null, results);
+//       }
+//     );
+//   },
+//   getUserByPhoneNumber: (phoneNumber, callBack) => {
+//     pool.query(
+//       `select * from transporter where phone_number = ?`,
+//       [phoneNumber],
+//       (error, results, fields) => {
+//         if (error) {
+//           callBack(error);
+//         }
+//         return callBack(null, results[0]);
+//       }
+//     );
+//   },
+//   getUserByUserId: (id, callBack) => {
+//     pool.query(
+//       `select * from transporter where id = ?`,
+//       [id],
+//       (error, results, fields) => {
+//         if (error) {
+//           callBack(error);
+//         }
+//         return callBack(null, results[0]);
+//       }
+//     );
+//   },
+//   getUsers: callBack => {
+//     pool.query(
+//       `select id, first_name, last_name, phone_number from transporter`,
+//       [],
+//       (error, results, fields) => {
+//         if (error) {
+//           callBack(error);
+//         }
+//         return callBack(null, results);
+//       }
+//     );
+//   },
 /*   updateUser: (data, callBack) => {
     pool.query(
       `update registration set firstName=?, lastName=?, gender=?, email=?, password=?, number=? where id = ?`,
@@ -87,4 +153,4 @@ module.exports = {
       }
     );
   } */
-};
+// };
